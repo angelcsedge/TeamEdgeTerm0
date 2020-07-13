60206540d4ff00ff4f3ea3f1c3d1d3b6b2e222f3
angelcsedge <67927942+angelcsedge@users.noreply.github.com>
2020-07-13 13:46:56 -0700

    Update variables.js

--- variables.js
+++ variables.js
@@ -18,13 +18,6 @@ It goes something like:
 
 console.log("------------------- Challenge 1 -------------------")
 
-let name = "Angel";
-let grade = "11";
-let funFact = "I own 10 birds";
-
-console.log(`Hi! My name is ${name}`);
-console.log(`I'm in the ${grade}th grade.`);
-console.log(`A fun fact about me is that ${funFact}`);
 
 
 // **** Challenge 1: Problem 1 ****
@@ -35,7 +28,7 @@ console.log(`A fun fact about me is that ${funFact}`);
 // **** Upchallenge! ****
 // Can you also print out the data type of the variables? 
 
-console.log('string');
+
 
 
 
@@ -43,15 +36,11 @@ console.log('string');
 // Create a string for the different lines of the Happy Birthday Song
 // Print out the song using the stings you declared.
 
-let song = "Happy Birthday to You, Happy Birthday to You, Happy Birthday Dear Chris, Happy Birthday to You.";
-
-consule.log(song)
 
 
 // **** Challenge 1: Problem 3 ****
 // Print out a string that has backticks in it.
 
-console.log('have a wonderful day');
 
 
 
@@ -73,44 +62,37 @@ let num2 = 10;
 // Store the sum of num1 and num2 in a variable.
 // Print the sum.
 
-let sum1 = num1 + num2;
-consule.log(sum1);
 
 
 // **** Challenge 2: Problem 2 ****
 // Store the difference between sum (above) and 7 in a variable.
 // Print the difference.
 
-let sum2 = sum1 - 7;
-consule.log(sum2);
+
 
 // **** Challenge 2: Problem 3 ****
 // Store the product of the difference and 3 in a variable.
 // Print the product.
 
-let sum3 = sum2 + 3;
-consule.log(sum3);
+
 
 // **** Challenge 2: Problem 4 ****
 // Store the power of the product squared in a variable.
 // Print the power.
 
-let sum4 = sqrt(sum3);
-consule.log(sum4);
+
 
 // **** Challenge 2: Problem 5 ****
 // Store the quotient of the power divided by 4 in a variable.
 // Print the quotient.
 
-let sum5 = sum3/4;
-consule.log(sum5);
+
 
 // **** Challenge 2: Problem 6 ****
 // Store the remainder of the quotient divided by 2 in a variable.
 // Print the remainder.
 
-let sum6 = sum5/2;
-consule.log(sum6);
+
 
 // **** Upchallenge! ****
 // Given a variable that stores a number, print the values that come right before and right after.
